3539e3653c350e8eec2ef887a20335dc4f1798dc
anwolosz <anwolosz@gmail.com>
2022-09-23 15:22:36 +0300

    index.html look

--- public/index.js
+++ public/index.js
@@ -45,3 +45,18 @@ const app = {
 };
 
 Vue.createApp(app).mount("#app");
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.previousElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
